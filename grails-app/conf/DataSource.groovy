cdce74d09306fb70e58dfa392fead4ba8711ff75
Rodrigo Peleias <=>
2014-02-25 21:03:45 -0300

    Remove postgreSql configs

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,9 @@
-dataSource {
+/*dataSource {
     pooled = true
     driverClassName = "org.h2.Driver"
     username = "sa"
     password = ""
-}
+}*/
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
